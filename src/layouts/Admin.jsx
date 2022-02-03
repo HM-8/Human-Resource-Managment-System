7ece85be581cf0db738b831777f2241ab262dd2f
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-02-03 13:02:45 +0300

    Remove fixed plugin

--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -5,7 +5,6 @@ import { useLocation, Route, Switch } from "react-router-dom";
 import AdminNavbar from "components/Navbars/AdminNavbar";
 import Footer from "components/Footer/Footer";
 import Sidebar from "components/Sidebar/Sidebar";
-import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
 import routes from "routes.js";
 import sidebarImage from "assets/img/sidebar-2.jpg";
 
@@ -56,14 +55,6 @@ function Admin() {
           <Footer />
         </div>
       </div>
-      <FixedPlugin
-        hasImage={hasImage}
-        setHasImage={() => setHasImage(!hasImage)}
-        color={color}
-        setColor={(color) => setColor(color)}
-        image={image}
-        setImage={(image) => setImage(image)}
-      />
     </>
   );
 }
