c7a2285f56a71bde41ca09b74f1e30cf62bea25f
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-02-03 16:01:21 +0300

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
  
 import React from "react";
 import ReactDOM from "react-dom";
-
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from '@material-ui/core';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
@@ -10,15 +10,24 @@ import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
 import "./assets/css/demo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+import { theme } from "./theme"
+import HomeLayout from "layouts/Home";
+import LoginLayout from "layouts/Login";
+import ForgotPasswordLayout from "layouts/ForgotPassword";
 import AdminLayout from "layouts/Admin";
 import EmployeeLayout from "layouts/Employee";
 
 ReactDOM.render(
+  <ThemeProvider theme={theme}>
   <BrowserRouter>
     <Switch>
+      <Route exact path="/" render={(props) => <HomeLayout {...props} />} />
+      <Route exact path="/login" render={(props) => <LoginLayout {...props} />} />
+      <Route exact path="/forgotpassword" render={(props) => <ForgotPasswordLayout {...props} />} />
       <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
       <Route path="/employee" render={(props) => <EmployeeLayout {...props} />} />
     </Switch>
-  </BrowserRouter>,
+  </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById("root")
 );
