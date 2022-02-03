a22b712f12013f5b7b817a03c205b6c6bb5aee8c
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-02-03 15:58:29 +0300

    Add theme

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@material-ui/core/styles'
+
+ export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#52c7b8',
+      main: '#F5793B',
+      dark: '#00675b',
+    },
+    secondary: {
+      light: '#ff9d3f',
+      main: '#F99E3E',
+      dark: '#b53d00',
+    },
+  },
+});
